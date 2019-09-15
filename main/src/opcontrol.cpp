3b08c9ba63a32eed3e4395805d0498963fa3a24f
Zain Lakhani <lakhani.zain786@gmail.com>
2019-09-15 15:35:00 -0400

    fixed andreas crap organization skills

--- main/src/opcontrol.cpp
+++ main/src/opcontrol.cpp
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "tracking.hpp"
+using namespace pros;
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -14,18 +16,61 @@
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+	  front_L.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+	  front_R.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+	  back_L.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+	  back_R.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+	  delay(6000);
+	  Task tracking_task(update);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+	  while (true){
+
+	     tracking.power_a = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+	     tracking.power_x = master.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
+	     tracking.power_y = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+
+	     if (fabs(tracking.power_a) < 5){
+	       tracking.power_a = 0;
+	     }
+	     if (fabs(tracking.power_x) < 5){
+	       tracking.power_x = 0;
+	     }
+	     if (fabs(tracking.power_y) < 5){
+	       tracking.power_y = 0;
+	     }
+
+	     move_drive(tracking.power_x, tracking.power_y, tracking.power_a);
+
+	     if (master.get_digital_new_press(E_CONTROLLER_DIGITAL_A)){
+	       tracking.x2 = tracking.xcoord;
+	       tracking.y2 = tracking.ycoord;
+	       tracking.a2 = tracking.global_angle;
+	     }
+	     if (master.get_digital_new_press(E_CONTROLLER_DIGITAL_B)){
+	       tracking.xcoord = 0;
+	       tracking.ycoord = 0;
+	       tracking.global_angle = 0;
+	     }
+
+			 if (master.get_digital(E_CONTROLLER_DIGITAL_Y)){
+	       tracking.move_to_target(tracking.x2, tracking.y2, tracking.a2);
+			 }
+
+	     if (master.get_digital(E_CONTROLLER_DIGITAL_RIGHT)){
+	       tracking.move_to_target(-13.0, 0.0, 0.0);
+	       delay(500);
+	       tracking.move_to_target(-13.0, 60.0, 0.0);
+	       tracking.move_to_target(-13.0, 80.0, M_PI/2);
+	       delay(500);
+	       tracking.move_to_target(6.0, 80.0, M_PI/2);
+	       delay(500);
+	       tracking.move_to_target(6.0, 80.0, M_PI);
+	       delay(500);
+	       tracking.move_to_target(-13.0, 80.0, M_PI);
+	       delay(500);
+	       tracking.move_to_target(0.0, 0.0, M_PI);
+	       delay(1000);
+	     }
+
+	  }
 	}
-}
---
+++ main/src/tracking.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "main.h"
+#include "config.hpp"
+#include <iostream>
+#include <cmath>
+#include <memory>
+#include <vector>
+using namespace std;
+using namespace pros;
+
+void move_drive(int x, int y, int a);
+void brake();
+void update(void *param);
+
+class Tracking {
+public:
+  double xcoord, ycoord, global_angle, power_a, power_x, power_y, x2 , y2 , a2  = 0;
+  bool toggle_target, toggle_cube, target, cube = false;
+  void move_to_target(double target_x, double target_y, double target_a);
+};
+
+extern Tracking tracking;
