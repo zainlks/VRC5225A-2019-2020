fe25f2288f134a3b5c477c079c7296f7f9a84b47
Zain Lakhani <lakhani.zain786@gmail.com>
2019-11-10 13:22:36 -0500

    Revert "red auto/ trying to figure out how to set up switch case for auto selection"
    This reverts commit 51a0c929b4abb37b3f192bc95005957ea160a62a.

--- main/src/autonomous.cpp
+++ main/src/autonomous.cpp
@@ -28,11 +28,6 @@ void intakeTask(void *param) {
       }
   }
 
-void auto_blue9();
-void auto_blue8();
-void auto_red9();
-void auto_red8();
-
 }
 void autonomous() {
   setDriveState(driveStates::Auto);
@@ -44,22 +39,6 @@ void autonomous() {
   printf("global angle:%f",tracking.global_angle);
   angler.move_absolute(1700, 200);
   intakeOn();
-
-//how to select auto_route at beginning of match?
-  // switch (auto_route){
-  //   case blue9: auto_blue9();
-  //   case blue8: auto_blue8();
-  //   case red9: auto_red9();
-  //   case red8: auto_red8();
-
-  printf("i am done\n");
-  printf("time is %d\n", autotimer-pros::millis());
-  master.clear();
-  delay(50);
-  master.print(2,0,"%d",millis()-autotimer);
-}
-
-void auto_blue9(){
   move_to_target_sync(0,23.5,0,false,75,false,true);
   brake();
   delay(100);
@@ -112,67 +91,10 @@ void auto_blue9(){
   while(angler.get_position()<ANGLER_TOP-50) delay(1);
   move_to_target_sync(-26,14, deg_to_rad(-135));
 
-}
-
-void auto_blue8(){
-
-}
-
-void auto_red9(){
-  move_to_target_sync(0,23.5,0,false,75,false,true);
-  brake();
-  delay(100);
-  move_drive(0,0,0);
-  fBar.move_absolute(towerHeights[1]-300, 200);
-  move_drive(0, -30, 0);
-  angler.move_absolute(400, 200);
-  while(fBar.get_position()<towerHeights[1]- 350 || angler.get_position()>1600) delay(1);
-  move_to_target_async(0, 25.25, 0, false, 75);
-  tracking.waitForDistance(0.75);
-  fBar.move_absolute(1, 200);
-  tracking.waitForComplete();
-  brake();
-  delay(100);
-  move_drive(0,0,0);
-  while(fBar.get_position() > 1000) delay(1);
-  move_to_target_async(0, 30.5, 0, false);
-  angler.move_absolute(1700, 200);
-  tracking.waitForComplete();
-  fBar.move(-10);
-  move_drive(0, -100, 0); //80
-  while(tracking.ycoord>18)delay(1);
-  move_to_target_sync(25.5, 2, 0, false,127);
-  move_drive(0, 80, 0); //80
-  while(tracking.ycoord<9)delay(1);
-  move_to_target_async(25.5, 45, 0, false, 70); //60
-  tracking.waitForDistance(18);
-  move_to_target_sync(25.5, 45, 0, false, 60); //50
-  tracking.waitForComplete();
-  delay(70);
-  move_to_target_async(31.75,9.75, deg_to_rad(135),false,127);
-  tracking.waitForDistance(12);
-  intakeL.move(-15);
-  intakeR.move(15);
-  tracking.waitForDistance(7);
-  angler.move_absolute(ANGLER_MID-1800, 200);
-  tracking.waitForComplete();
-  brake()
-  delay(75);
-  move_drive(0,0,0);
-  angler.move_absolute(ANGLER_TOP-1000, 100);
-  intakeL.move(15);
-  intakeR.move(-15);
+  printf("i am done\n");
+  printf("time is %d\n", autotimer-pros::millis());
+  master.clear();
   delay(50);
-  tracking.LSLineup();
-  angler.move_absolute(ANGLER_TOP, 120);
-  while((intakeL.get_actual_velocity()>1 || intakeR.get_actual_velocity()>1) && angler.get_position()<ANGLER_TOP-250) delay(1);
-  intakeL.move(-10);
-  intakeR.move(10);
-  while(angler.get_position()<ANGLER_TOP-50) delay(1);
-  move_to_target_sync(26,14, deg_to_rad(135));
-
-}
-
-void auto_red8(){
+  master.print(2,0,"%d",millis()-autotimer);
 
 }
