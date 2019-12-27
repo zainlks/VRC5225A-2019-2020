d7c1ed16e90fa6f66811a44caa80eb805dfae46c
Mike Stefan <mikeGstefan66@gmail.com>
2019-12-27 11:11:51 -0500

    added up down

---
+++ testing/up-down/src/config.cpp
@@ -0,0 +1,12 @@
+#include "config.hpp"
+ADIEncoder leftencoder (1, 2, true);
+ADIEncoder rightencoder (5, 6, false);
+ADIEncoder backencoder (3, 4, true);
+ADIAnalogIn leftLs(7);
+ADIAnalogIn rightLs(8);
+Vision vision(5);
+Motor intakeR(6,false), intakeL(2,false);
+Motor angler(4,true);
+Motor fBar(3, true);
+Motor front_L(1, false), front_R(10, true), back_L(11, false), back_R(20, true);
+Controller master(E_CONTROLLER_MASTER);
---
+++ testing/up-down/src/config.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "main.h"
+
+using namespace pros;
+
+extern ADIEncoder leftencoder;
+extern ADIEncoder rightencoder;
+extern ADIAnalogIn leftLs;
+extern ADIAnalogIn rightLs;
+extern ADIEncoder backencoder;
+extern Vision vision;
+extern Motor intakeR, intakeL;
+extern Motor angler;
+extern Motor fBar;
+extern Motor front_L, front_R, back_L, back_R;
+extern Controller master;
